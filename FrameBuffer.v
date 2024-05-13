200166dc92969b8e0c7225a46997e2b6ec9618b8
Marc Hyeler <birdpump@gmail.com>
2024-05-12 23:18:23 -0700

    V3 revert to old frame buffer

--- FrameBuffer.v
+++ FrameBuffer.v
@@ -3,74 +3,39 @@ module FrameBuffer (
     input wire clear,
     input wire swap,
     input wire draw,
+    input wire [307200:0] frame,
     input wire [9:0] position_x,
     input wire [9:0] position_y,
-	 
-    input wire [9:0] position_x_new,
-    input wire [9:0] position_y_new,
-    input wire [7:0] color_r_new,
-    input wire [7:0] color_g_new,
-    input wire [7:0] color_b_new,
-	 
     output reg [7:0] output_r,
     output reg [7:0] output_g,
     output reg [7:0] output_b
 );
 
-// Frame buffer memories for double buffering
-reg [23:0] frame_buffer_1 [0:639][0:479];
-reg [23:0] frame_buffer_2 [0:639][0:479];
-
-// Current active buffer selection
-reg active_buffer = 0;
+// Frame buffer memory
+reg [23:0] frame_buffer [0:639][0:479];
 
 
 
 
-// Write to frame buffer based on the active buffer
+
+// Write to frame buffer
+integer i;
 always @(posedge clk) begin
-	  // Write to the active buffer
-	  if (active_buffer) begin
-			frame_buffer_1[position_x_new][position_y_new] <= {color_r_new, color_g_new, color_b_new};
-	  end else begin
-			frame_buffer_2[position_x_new][position_y_new] <= {color_r_new, color_g_new, color_b_new};
-	  end
+	 for (i=0; i<200; i=i+1) frame_buffer[i][i] <= {8'b11111111, 8'b00000000, 8'b00000000}; // Red color;
 end
 
-
-
-// Read from frame buffer based on the active buffer and send values to display
+// Read from frame buffer
 always @* begin
     if (draw) begin
-        // Read from the active buffer
-        if (active_buffer) begin
-            output_r = frame_buffer_2[position_x][position_y][23:16];
-            output_g = frame_buffer_2[position_x][position_y][15:8];
-            output_b = frame_buffer_2[position_x][position_y][7:0];
-        end else begin
-            output_r = frame_buffer_1[position_x][position_y][23:16];
-            output_g = frame_buffer_1[position_x][position_y][15:8];
-            output_b = frame_buffer_1[position_x][position_y][7:0];
-        end
+        // Output pixel color at specified coordinates
+        output_r = frame_buffer[position_x][position_y][23:16];
+        output_g = frame_buffer[position_x][position_y][15:8];
+        output_b = frame_buffer[position_x][position_y][7:0];
     end else begin
         // Output black if draw signal is low
         output_r = 8'b00000000;
         output_g = 8'b00000000;
         output_b = 8'b00000000;
-    end
-end
-
-
-// Switch between frame buffers based on the swap signal
-always @(posedge clk) begin
-    if (clear) begin
-        // Initialize with buffer 1 as active
-        active_buffer <= 0;
-    end else begin
-        if (swap) begin
-            // Toggle active buffer
-            active_buffer <= ~active_buffer;
-        end
     end
 end
 
